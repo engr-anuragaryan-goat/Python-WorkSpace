fcc25b95030361890dfd7408881d94baa95aaafe
Anurag Aryan <anuragaryan200608@gmail.com>
2026-04-22 13:13:35 +0530

    Adding new sub folder to my parent repo.

---
+++ functional-programming/Function/fun_1.py
@@ -0,0 +1,9 @@
+#WAP to create a function for adding to number
+
+#function Defination
+def ad(x,y):
+    return (x+y)
+n1=int(input("Enter a number : "))
+n2=int(input("Enter a number : "))
+result=ad(n1,n2)
+print("Result=",result)
